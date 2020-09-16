c23be978d486f188f0ccefd17f1b8130e382df42
AdvanDias <advan_dias@hotmail.com>
2020-09-16 18:06:46 -0300

    commit telas de listas de eventos. criou arquivos html tambem

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from core.models import Evento
 # Create your views here.
+
+#def index(request):
+#    return redirect('/agenda/')
+
+def lista_eventos(request):
+    # filtrando tudo
+    evento = Evento.objects.all()
+    #filtrando pelo usuario
+    usuario = request.user
+    #evento = Evento.objects.filter(usuario=usuario) -- filtrando pelo usuario
+    response = {'evento': evento}
+    return render(request,'agenda.html', response)
